7502239ee067758700badec37f703665e6c31eb2
Gourav Pullela <gourav.pullela@gmail.com>
2023-11-07 23:52:43 -0500

    Quartz sync: Nov 7, 2023, 11:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: UA-141670257-1
     },
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates"],
